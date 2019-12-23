dc5c358d2ebec01f73076b08f968bb713a71d746
DAHEE <53304263+daheewoo@users.noreply.github.com>
2019-12-23 21:20:57 +0900

    first

---
+++ main/CalendarMainController.java
@@ -0,0 +1,19 @@
+package theBug.calendar.main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class CalendarMainController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
